5787bc545ab9897905851a4ec8618bb260b51be5
Norberts <mainanorbert@gmail.comm>
2024-03-04 20:27:24 +0300

    making variables immutable

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -3,7 +3,9 @@ export default function taskBlock (trueOrFalse) {
   let task2 = true;
 
   if (trueOrFalse) {
+    // eslint-disable-next-line no-unused-vars, no-shadow
     task = true;
+    // eslint-disable-next-line no-unused-vars, no-shadow
     task2 = false;
   }
 

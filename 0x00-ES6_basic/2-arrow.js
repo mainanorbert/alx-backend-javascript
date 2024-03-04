06a8d7347a9bd712440339a5167128424c8dc007
Norberts <mainanorbert@gmail.comm>
2024-03-04 21:07:01 +0300

    arrow func

--- 0x00-ES6_basic/2-arrow.js
+++ 0x00-ES6_basic/2-arrow.js
@@ -1,9 +1,8 @@
-export default function getNeighborhoodsList () {
+export default function getNeighborhoodsList() {
   this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
 
-  const self = this;
   this.addNeighborhood = (newNeighborhood) => {
-    self.sanFranciscoNeighbourhoods.push(newNeighborhood);
-    return self.sanFranciscoNeighborhoods;
+    this.sanFranciscoNeighbourhoods.push(newNeighborhood);
+    return this.sanFranciscoNeighborhoods;
   };
 }

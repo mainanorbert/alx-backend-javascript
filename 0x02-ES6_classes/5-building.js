56e5a990a0b8be4848cc263b3d9f5c8f5a9f6874
Norberts <mainanorbert@gmail.comm>
2024-03-07 15:58:42 +0300

    building class

---
+++ 0x02-ES6_classes/5-building.js
@@ -0,0 +1,16 @@
+/* eslint-disable class-methods-use-this */
+class Building {
+  constructor(sqft) {
+    this._sqft = sqft;
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+
+  evacuationWarningMessage() {
+    throw new Error('Class extending Building must override evacuationWarningMessage');
+  }
+}
+
+export default Building;

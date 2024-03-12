68936447bf87013d195276e3240fcb41ecdfae97
Norberts <mainanorbert@gmail.comm>
2024-03-12 09:39:55 +0300

    implenting a new grad

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -3,7 +3,7 @@ export default function updateStudentGradeByCity(students, city, newGrades) {
     .filter((student) => student.location === city)
     .map((student) => {
       const newGradeObj = newGrades.find((grade) => grade.studentId === student.id);
-      const grade = newGradeObj ? newGradeObj.grade : 'NA';
+      const grade = newGradeObj ? newGradeObj.grade : 'N/A';
       return { ...student, grade };
     });
 }

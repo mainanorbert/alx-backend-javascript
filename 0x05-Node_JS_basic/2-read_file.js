eea10ac5d406cc1e0fbbec51573e73a78312140f
Norberts <mainanorbert@gmail.comm>
2024-04-30 08:11:54 +0300

    reading csv file

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,11 +1,14 @@
 const fs = require('fs');
+
 const countStudents = (path) => {
   try {
     const data = fs.readFileSync(path, 'utf8');
     const lines = data.split('\n');
     const records = lines.slice(1).filter((line) => line.trim() !== '').map((line) => {
       const [firstname, lastname, age, field] = line.split(',');
-      return { firstname, lastname, age: parseInt(age), field };
+      return {
+        firstname, lastname, age: parseInt('10', age), field
+      };
     });
     const len = records.length;
     console.log(`Number of students: ${len}`);
@@ -18,9 +21,11 @@ const countStudents = (path) => {
       return acc;
     }, {});
     for (const field in studentByField) {
-      const noOfStud = studentByField[field].length;
-      const studentsNames = studentByField[field].join(', ');
-      console.log(`Number of students in ${field}: ${noOfStud}. List: ${studentsNames}`);
+      if (Object.prototype.hasOwnProperty.call(studentByField, field)) {
+        const noOfStud = studentByField[field].length;
+        const studentsNames = studentByField[field].join(', ');
+        console.log(`Number of students in ${field}: ${noOfStud}. List: ${studentsNames}`);
+      }
     }
   } catch (error) {
     throw new Error('Cannot load the database');

40dd7cf36daecac41f5c79814ccf2b70862cdde5
Norberts <mainanorbert@gmail.comm>
2024-04-30 21:52:08 +0300

    express

--- 0x05-Node_JS_basic/6-http_express.js
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -4,6 +4,20 @@ const app = express();
 app.get('/', (req, res) => {
   res.send('Hello Holberton School!');
 });
+app.use((req, res) => {
+  res.status(404).send(`
+    <!DOCTYPE html>
+    <html lang="en">
+      <head>
+        <meta charset="utf-8">
+        <title>Error</title>
+      </head>
+      <body>
+        <pre>Cannot GET ${req.url}</pre>
+      </body>
+    </html>
+  `);
+});
 app.listen(1245, () => {
   console.log('Server running at http://localhost:1245/');
 });

e4d84059156a0ec573869de65bd2097a3db97f83
Norberts <mainanorbert@gmail.comm>
2024-04-30 05:52:22 +0300

    Using Process stdin

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,7 +1,9 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 process.stdin.on('readable', () => {
   const myname = process.stdin.read();
-  process.stdout.write(`Your Name is: ${myname}\n`);
+  if (myname) {
+    process.stdout.write(`Your Name is: ${myname}\n`);
+  }
 });
 process.stdin.on('end', () => {
   process.stdout.write('This important software is now closing\n');

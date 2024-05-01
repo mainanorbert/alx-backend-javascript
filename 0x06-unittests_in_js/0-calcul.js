49cdf16afb259361548b503c38a08deb91f8fc04
Norberts <mainanorbert@gmail.comm>
2024-05-01 22:34:25 +0300

    Mocha and Node assertion library

--- 0x06-unittests_in_js/0-calcul.js
+++ 0x06-unittests_in_js/0-calcul.js
@@ -1,7 +1,7 @@
-function add (a, b) {
+function calculateNumber (a, b) {
   const roundA = Math.round(a);
   const roundB = Math.round(b);
   return roundA + roundB;
 }
 
-module.exports = { add };
+module.exports = { calculateNumber };
--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -1,18 +1,18 @@
 const assert = require('assert');
-const { add } = require('./0-calcul');
+const { calculateNumber } = require('./0-calcul');
 
 describe('add', () => {
   it('returns sum', () => {
-    assert.equal(add(1.4, 1.5), 3);
-    assert.equal(add(1, 1), 2);
+    assert.equal(calculateNumber(1.4, 1.5), 3);
+    assert.equal(calculateNumber(1, 1), 2);
   });
 
   it('sum for negatives', () => {
-    assert.equal(add(-1, -1), -2);
-    assert.equal(add(-1.4, -1.5), -2);
+    assert.equal(calculateNumber(-1, -1), -2);
+    assert.equal(calculateNumber(-1.4, -1.5), -2);
   });
   it('testing - and +', () => {
-	  assert.equal(add(-1, 1), 0)
-	  assert.equal(add(-1.5, 2), 1)
+	  assert.equal(calculateNumber(-1, 1), 0)
+	  assert.equal(calculateNumber(-1.5, 2), 1)
   })
 });

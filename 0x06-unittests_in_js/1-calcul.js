156c1014dc7d6a25cda71d7c7616c0f283938e6c
Norberts <mainanorbert@gmail.comm>
2024-05-01 22:57:39 +0300

    more tests

---
+++ 0x06-unittests_in_js/1-calcul.js
@@ -0,0 +1,21 @@
+const calculateNumber = (a, b, type) => {
+  const roundA = Math.round(a);
+  const roundB = Math.round(b);
+
+  switch (type) {
+    case 'ADD':
+      return roundA + roundB;
+    case 'SUBTRACT':
+      return roundA - roundB
+    case 'DIVIDE':
+      if (roundB === 0){
+        return 'Error'
+      }
+      return roundA/roundB  
+    default:
+      throw new Error('Invalid type. Type must be SUM, SUBTRACT, or DIVIDE.');
+  }
+
+}
+
+module.exports = calculateNumber;

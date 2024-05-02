76ae4d48f7f4e6f7a92b3b6f516a175e4898c947
Norberts <mainanorbert@gmail.comm>
2024-05-02 13:23:51 +0300

    chai

--- 0x06-unittests_in_js/2-calcul_chai.test.js
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -2,21 +2,21 @@ const { expect } = require('chai');
 const calculateNumber = require('./2-calcul_chai.js');
 
 describe('calculateNumber', () => {
-    describe('SUM', () => {
-        it('return the sum of rounded numbers, up & down', () => {
-            expect(calculateNumber('SUM', 1.46, 4.5)).to.equal(6);
+    describe('SUM operation', () => {
+        it('should return the sum of rounded numbers, up & down', () => {
+            expect(calculateNumber('SUM', 1.4, 4.5)).to.equal(6);
         });
 
-        it('sum of rounded numbers, both up', () => {
-            expect(calculateNumber('SUM', 1.56, 4.5)).to.equal(7);
+        it('should return the sum of rounded numbers, both up', () => {
+            expect(calculateNumber('SUM', 1.54, 4.5)).to.equal(7);
         });
 
         it('should return the sum of zero and positive number', () => {
-            expect(calculateNumber('SUM', 0, 4.53)).to.equal(5);
+            expect(calculateNumber('SUM', 0, 4.5)).to.equal(5);
         });
 
-        it('return the sum of zero and negative number', () => {
-            expect(calculateNumber('SUM', 0, -4.52)).to.equal(-4);
+        it('should return the sum of zero and negative number', () => {
+            expect(calculateNumber('SUM', 0, -4.5)).to.equal(-4);
         });
     });
 
@@ -43,11 +43,11 @@ describe('calculateNumber', () => {
             expect(calculateNumber('DIVIDE', 1.4, 4.5)).to.equal(0.2);
         });
 
-        it('Returns "Error" when divisor is zero', () => {
+        it('should return "Error" when divisor is zero', () => {
             expect(calculateNumber('DIVIDE', 1.4, 0)).to.equal('Error');
         });
 
-        it('Returns the division result of negative numbers when divisor is not zero', () => {
+        it('should return the division result of negative numbers when divisor is not zero', () => {
             expect(calculateNumber('DIVIDE', -1.4, -4.5)).to.equal(0.25);
         });
 
@@ -55,7 +55,7 @@ describe('calculateNumber', () => {
             expect(calculateNumber('DIVIDE', 0, 4.5)).to.equal(0);
         });
 
-        it('returns the division result of zero and negative number when divisor is not zero', () => {
+        it('should return the division result of zero and negative number when divisor is not zero', () => {
             expect(calculateNumber('DIVIDE', 0, -4.5)).to.equal(-0);
         });
     });

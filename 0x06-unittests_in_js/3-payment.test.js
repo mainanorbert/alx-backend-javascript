9788995ac70991b38ef79e29b5811fc08b432d26
Norberts <mainanorbert@gmail.comm>
2024-05-02 14:20:47 +0300

    spies

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -1,19 +1,45 @@
-const { expect } = require('chai');
+// const { expect } = require('chai');
+// const sinon = require('sinon');
+// const Utils = require('./utils');
+// const sendPaymentRequestToApi = require('./3-payment');
+
+// describe('sendPaymentRequestToApi', () => {
+//     it('testing 2 function for similar output', () => {
+//         const calculateNumberSpy = sinon.spy(Utils, 'calculateNumber');
+//         const consoleSpy = sinon.spy(console, 'log');
+//         sendPaymentRequestToApi(100, 20);
+//         expect(calculateNumberSpy.calledOnce).to.be.true;
+//         expect(calculateNumberSpy.calledWith(100, 20, 'SUM')).to.be.true;
+//         expect(consoleSpy.calledOnce).to.be.true;
+//         expect(consoleSpy.calledWith('The total is: 120')).to.be.true;
+//         calculateNumberSpy.restore();
+//         consoleSpy.restore();
+
+//     })
+// })
+
 const sinon = require('sinon');
-const Utils = require('./utils');
+const assert = require('assert');
 const sendPaymentRequestToApi = require('./3-payment');
+const Utils = require('./utils');
 
 describe('sendPaymentRequestToApi', () => {
-    it('testing 2 function for similar output', () => {
-        const calculateNumberSpy = sinon.spy(Utils, 'calculateNumber');
-        const consoleSpy = sinon.spy(console, 'log');
-        sendPaymentRequestToApi(100, 20);
-        expect(calculateNumberSpy.calledOnce).to.be.true;
-        expect(calculateNumberSpy.calledWith(100, 20, 'SUM')).to.be.true;
-        expect(consoleSpy.calledOnce).to.be.true;
-        expect(consoleSpy.calledWith('The total is: 120')).to.be.true;
-        calculateNumberSpy.restore();
-        consoleSpy.restore();
+    let calculateNumberSpy;
 
-    })
-})
+    beforeEach(() => {
+        calculateNumberSpy = sinon.spy(Utils, 'calculateNumber');
+    });
+
+    afterEach(() => {
+        calculateNumberSpy.restore();
+    });
+
+    it('should call Utils.calculateNumber with correct arguments and log the result', () => {
+        const totalAmount = 100;
+        const totalShipping = 20;
+
+        sendPaymentRequestToApi(totalAmount, totalShipping);
+
+        assert(calculateNumberSpy.calledOnceWithExactly('SUM', totalAmount, totalShipping));
+    });
+});

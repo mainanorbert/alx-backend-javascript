48a1503d8bfc6ec1d34d20733b263e24b90d1d6c
Norberts <mainanorbert@gmail.comm>
2024-05-02 17:56:11 +0300

    spies

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -1,23 +1,3 @@
-// const { expect } = require('chai');
-// const sinon = require('sinon');
-// const Utils = require('./utils');
-// const sendPaymentRequestToApi = require('./3-payment');
-
-// describe('sendPaymentRequestToApi', () => {
-//     it('testing 2 function for similar output', () => {
-//         const calculateNumberSpy = sinon.spy(Utils, 'calculateNumber');
-//         const consoleSpy = sinon.spy(console, 'log');
-//         sendPaymentRequestToApi(100, 20);
-//         expect(calculateNumberSpy.calledOnce).to.be.true;
-//         expect(calculateNumberSpy.calledWith(100, 20, 'SUM')).to.be.true;
-//         expect(consoleSpy.calledOnce).to.be.true;
-//         expect(consoleSpy.calledWith('The total is: 120')).to.be.true;
-//         calculateNumberSpy.restore();
-//         consoleSpy.restore();
-
-//     })
-// })
-
 const sinon = require('sinon');
 const assert = require('assert');
 const sendPaymentRequestToApi = require('./3-payment');

a9b3d14fa7fe9d4a6c94ef16255ff7649951859a
Norberts <mainanorbert@gmail.comm>
2024-05-03 08:25:51 +0300

    Regex integration testing

--- 0x06-unittests_in_js/9-api/api.test.js
+++ 0x06-unittests_in_js/9-api/api.test.js
@@ -25,4 +25,12 @@ describe('Cart page', () => {
       done();
     });
   });
+
+  it('Correct status code when :id is NOT a number (=> 404)', (done) => {
+    request.get('http://localhost:7865/cart/hello', (error, response, body) => {
+      if (error) return done(error);
+      expect(body).to.be.equal('Cannot GET /cart/hello');
+      done();
+    });
+  });
 });

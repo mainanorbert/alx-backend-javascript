a76ad10a1fc736279db00644613f4eef85d4d774
Norberts <mainanorbert@gmail.comm>
2024-05-03 08:31:54 +0300

    Regex integration testing

--- 0x06-unittests_in_js/9-api/api.test.js
+++ 0x06-unittests_in_js/9-api/api.test.js
@@ -26,7 +26,7 @@ describe('Cart page', () => {
     });
   });
 
-  it('Correct status code when :id is NOT a number (=> 404)', (done) => {
+  it('Correct result when :id is NOT a number (=> 404)', (done) => {
     request.get('http://localhost:7865/cart/hello', (error, response, body) => {
       if (error) return done(error);
       expect(body).to.be.equal('Cannot GET /cart/hello');

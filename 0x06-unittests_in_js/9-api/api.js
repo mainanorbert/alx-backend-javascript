5d36ff1c060c1d9b8eaf96ac543da25fd66ba668
Norberts <mainanorbert@gmail.comm>
2024-05-03 21:58:26 +0300

    Regex integration testing

--- 0x06-unittests_in_js/9-api/api.js
+++ 0x06-unittests_in_js/9-api/api.js
@@ -1,14 +1,24 @@
 const express = require('express');
+
 const app = express();
 
-app.get('/', (req, res) =>{
-    res.send('Welcome to the payment system')
+const PORT = 7865;
+
+app.listen(PORT, () => {
+  console.log(`API available on localhost port ${PORT}`);
 });
+
+app.get('/', (req, res) => {
+  res.send('Welcome to the payment system');
+});
+
 app.get('/cart/:id(\\d+)', (req, res) => {
-    const cartId = req.params.id;
-    res.status(200).send(`Payment methods for cart ${cartId}\n`)
-})
-const server = app.listen(7865, ()=>{
-    console.log('API available on localhost port 7865');
+  const { id } = req.params;
+  res.send(`Payment methods for cart ${id}`);
 });
-module.exports = server
+
+app.use((req, res) => {
+  res.status(404).send('Cannot GET ' + req.path);
+});
+
+module.exports = app;
